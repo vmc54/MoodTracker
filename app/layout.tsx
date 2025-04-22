06bec1ef7b41205a577ac824151c951a012265e7
vmc54 <vmc54@cornell.edu>
2025-04-22 18:12:35 -0400

    lightmode

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en" suppressHydrationWarning>
-            <body className="font-sans antialiased dark">
+            <body className="font-sans antialiased bg-white text-black">
                 <Providers>
                     <div className="flex min-h-svh flex-col">
                         <Header />
