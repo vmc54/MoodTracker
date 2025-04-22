b995775748dbbc853ddf1104397bfba523770647
vmc54 <vmc54@cornell.edu>
2025-04-22 15:44:18 -0400

    commit

--- src/database/schema.ts
+++
@@ -1,7 +0,0 @@
-import { pgTable, text, date } from 'drizzle-orm/pg-core';
-
-export const moods = pgTable('moods', {
-  userId: text().notNull(),
-  mood: text().notNull(),
-  date: date().notNull()
-});
